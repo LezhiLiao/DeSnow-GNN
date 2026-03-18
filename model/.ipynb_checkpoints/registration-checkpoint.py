9f3f51e7c5d8cde6b34cf1a85b279b77bb4c9c16
unknown <1529117211@qq,com>
2026-03-18 15:27:34 +0800

    Remove cache files and add gitignore

--- model/.ipynb_checkpoints/registration-checkpoint.py
+++
@@ -1,8 +0,0 @@
-def registration(x,edge_index,registration_dis):#out就是xj
-
-    x_i = x[:,0:3][edge_index[1]]
-    x_j = x[:,0:3][edge_index[0]]
-    x_r = registration_dis[edge_index[1]]
-    result = x_j - x_i + x_r
-
-    return result
